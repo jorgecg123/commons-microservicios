2791c05c612b17ed8fe6e69545daf644b381da04
jorge.cruz <jorge.cruz@elektra.com.mx>
2021-07-27 17:57:41 -0500

    first commit

---
+++ src/main/java/com/formacionbdi/microservicios/commons/service/CommonService.java
@@ -0,0 +1,26 @@
+package com.formacionbdi.microservicios.commons.service;
+
+import java.util.Optional;
+
+
+public interface CommonService<E> {
+
+	public Iterable<E> findAll();
+	
+	public Optional<E> findById(Long id);
+	
+	public E save(E entity);
+	
+	public void deleteById(Long id);
+}
+
+//public interface AlumnoService {
+//
+//	public Iterable<Alumno> findAll();
+//	
+//	public Optional<Alumno> findById(Long id);
+//	
+//	public Alumno save(Alumno alumno);
+//	
+//	public void deleteById(Long id);
+//}
